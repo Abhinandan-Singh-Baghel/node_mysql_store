a84ca220b00894f1a786a911b6a2df90c9cf5da4
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2023-09-01 23:13:07 +0530

    Initial commit

---
+++ bamazonCustomer.js
@@ -0,0 +1,117 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const divider = "\n--------------------------------------------------------------------------------------------";
+
+const idNumberArray = [];
+const idChoiceArray = [];
+const quantityInStockArray = [];
+const quantityRequestedArray = [];
+const selectedItemPriceArray = [];
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "B!gRed1988",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    readAllProducts();
+});
+
+function readAllProducts() {
+    console.log(divider + "\nWELCOME TO BAMAZON!\n" + divider);
+    connection.query("SELECT * from products", function (err, res) {
+        if (err) throw err;
+        for (i = 0; i < res.length; i++) {
+            idNumberArray.push(res[i].item_id);
+        }
+        console.table(res);
+        console.log(divider);
+        singleItemChoice();
+    });
+}
+
+function singleItemChoice() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "idQuery",
+            message: "What is the ID of the product you would like to buy?"
+        }]).then(answers => {
+            if (!isNaN(answers.idQuery)) {
+                const numberValue = parseInt(answers.idQuery);
+                const isInArray = idNumberArray.includes(numberValue);
+                if (isInArray) {
+                    console.log("\n\nHere is your item!" + divider);
+                    connection.query("SELECT * from products WHERE item_id = " + answers.idQuery, function (err, res) {
+                        if (err) throw err;
+                        console.table(res);
+                        console.log(divider);
+                        idChoiceArray.push(numberValue);
+                        selectedItemPriceArray.push(res[0].item_price);
+                        quantityChoice();
+                    });
+                } else {
+                    console.log("\n\n  That ID does not match a product in our system. Please try again.\n\n");
+                    singleItemChoice();
+                }
+            } else {
+                console.log("\n\n  Bamazon only uses numbers to identify their products. Please try again.\n\n");
+                singleItemChoice();
+            }
+        });
+}
+
+function quantityChoice() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "quantityQuery",
+            message: "How many of this item would you like?"
+        }
+    ]).then(answers => {
+        console.log(divider);
+        const quanParseInt = parseInt(answers.quantityQuery);
+        connection.query("SELECT stock_quantity from products WHERE item_id = " + idChoiceArray[0], function (err, res) {
+            if (err) throw err;
+            if (!isNaN(answers.quantityQuery)) {
+                if (quanParseInt <= res[0].stock_quantity) {
+                    quantityInStockArray.push(res[0].stock_quantity);
+                    quantityRequestedArray.push(answers.quantityQuery);
+                    updateProduct();
+                } else {
+                    console.log("I'm sorry, we do not have that many of your requested item in stock! Please try again!\n");
+                    quantityChoice();
+                }
+            } else {
+                console.log("\n\n  Bamazon only uses numbers to identify their quantities. Please try again.\n\n");
+                quantityChoice();
+            }
+        });
+    });
+};
+
+function updateProduct() {
+    console.log("Thank you for your purchase!");
+    const query = connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+            {
+                stock_quantity: quantityInStockArray[0] - quantityRequestedArray[0]
+            },
+            {
+                item_id: idChoiceArray[0]
+            }
+        ],
+        function (err, res) {
+            const totalPrice = selectedItemPriceArray[0] * quantityRequestedArray[0];
+            const result = (Math.round(totalPrice * 100) / 100).toFixed(2);
+            console.log(divider + `\nThe total price of your purchase is $${result}.\nHave a fantastic day!`);
+            connection.end();
+        });
+}
---
+++ bamazonManager.js
@@ -0,0 +1,184 @@
+// dependencies
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+const divider = "\n------------------------------------------------------------------------------------------";
+const idNumberArray = [];
+let idChoiceArray = [];
+let quantityInStockArray = [];
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "B!gRed1988",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    pushIdNumbers();
+    managerOptions("Please select from the following:");
+});
+
+function managerOptions(message) {
+    idChoiceArray = [];
+    quantityInStockArray = [];
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "managerChoice",
+            message: message,
+            choices: [
+                "Products for Sale",
+                "Low Inventory",
+                "Add to Inventory",
+                "Add New Product",
+                "Quit"
+            ]
+        }]).then(answers => {
+            switch (answers.managerChoice) {
+                case "Products for Sale":
+                    showAllProducts();
+                    break;
+                case "Low Inventory":
+                    showLowInventory();
+                    break;
+                case "Add to Inventory":
+                    itemChoice();
+                    break;
+                case "Add New Product":
+                    addNewProduct();
+                    break;
+                case "Quit":
+                    console.log("\nHave a great day!");
+                    connection.end();
+            }
+        });
+}
+
+function pushIdNumbers() {
+    connection.query("SELECT * from products", function (err, res) {
+        if (err) throw err;
+        for (i = 0; i < res.length; i++) {
+            idNumberArray.push(res[i].item_id);
+        }
+    });
+}
+
+function showAllProducts() {
+    connection.query("SELECT * from products", function (err, res) {
+        if (err) throw err;
+        console.log(divider + "\nPRODUCTS IN STOCK\n" + divider);
+        console.table(res);
+        console.log(divider);
+        managerOptions("What would like to do now?");
+    });
+};
+
+
+function showLowInventory() {
+    console.log(divider + "\nPRODUCTS WITH 5 ITEMS OR LESS IN STOCK\n" + divider);
+    connection.query("SELECT * from products WHERE stock_quantity <= 5", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        console.log(divider);
+        managerOptions("What would you like to do now?");
+    });
+}
+
+function itemChoice() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "idQuery",
+            message: "What is the ID of the product you would like to add quantity to?"
+        }]).then(answers => {
+            if (!isNaN(answers.idQuery)) {
+                const idParsedValue = parseInt(answers.idQuery);
+                const isInArray = idNumberArray.includes(idParsedValue);
+                if (isInArray) {
+                    connection.query("SELECT * from products WHERE item_id = " + answers.idQuery, function (err, res) {
+                        if (err) throw err;
+                        idChoiceArray.push(idParsedValue);
+                        quantityInStockArray.push(res[0].stock_quantity);
+                        addInventory();
+                    });
+                } else {
+                    console.log("\n\n  That ID does not match a product in our system. Please try again.\n\n");
+                    itemChoice();
+                }
+            } else {
+                console.log("\n\n  Bamazon only uses numbers to identify their products. Please try again.\n\n");
+                itemChoice();
+            }
+        });
+}
+
+function addInventory() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "quantityQuery",
+            message: "How much of this item would you like to add?"
+        }]).then(answers => {
+            let quantityParsedValue = parseInt(answers.quantityQuery);
+            const newTotalStock = quantityInStockArray[0] + quantityParsedValue;
+            console.log("\nUpdating stock quantity...");
+            const query = connection.query(
+                "UPDATE products SET ? WHERE ?",
+                [
+                    {
+                        stock_quantity: newTotalStock
+                    },
+                    {
+                        item_id: idChoiceArray[0]
+                    }
+                ],
+                function (err, res) {
+                    if (err) throw err;
+                    console.log(divider + `\nProduct #${idChoiceArray[0]} updated successfully!
+                    \nYour new quantity total is ${newTotalStock}!\n`);
+                    managerOptions("What would you like to do now?");
+                });
+        });
+
+}
+
+function addNewProduct() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "productName",
+            message: "What is the name of the product?"
+        },
+        {
+            type: "input",
+            name: "departmentName",
+            message: "What is the department name of the product?"
+        },
+        {
+            type: "input",
+            name: "price",
+            message: "How much does this item cost per unit?"
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How much of this item would you like to add?"
+        }
+    ]).then(answers => {
+        const productName = answers.productName;
+        const departmentName = answers.departmentName;
+        const price = answers.price;
+        const quantity = answers.quantity;
+        connection.query(`INSERT INTO products (product_name, department_name, item_price, stock_quantity) 
+        VALUES ("${productName}", "${departmentName}", "${price}", "${quantity}")`, function (err, res) {
+            if (err) throw err;
+            console.log(`\n${productName} was added successfully!\n`);
+            managerOptions("What would you like to do now?");
+        });
+    });
+};
